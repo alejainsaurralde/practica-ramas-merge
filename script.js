3e6de2d80b6d70f01fb7555d8cf13cd3b9ad9816
Alejandra Insaurralde <alejainsaurralde@gmail.com>
2024-05-24 01:26:43 -0300

    Solución botón deshacer

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function agregarALaPantalla(value) {
     document.getElementById('pantalla').value += value;
-  }
-  
-  function calcular() {
+}
+
+function calcular() {
     const valorPantalla = document.getElementById('pantalla').value;
     const result = eval(valorPantalla);
     document.getElementById('pantalla').value = result;
-  }
-  
-  function limpiarPantalla() {
+}
+
+function limpiarPantalla() {
     document.getElementById('pantalla').value = '';
-  }
-  
+}
+
+function deshacer() {
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = valorPantalla.substring(0, valorPantalla.length - 1)
+    document.getElementById('pantalla').value = resultado
+}
